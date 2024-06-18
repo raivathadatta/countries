baaa95aded12c82a1c235b2181a22f25225e2e6f
raivatha datta <raivatha.28.2@mountblue.tech>
2024-06-18 10:42:16 +0530

    react icons moon and sun added

--- countries-react/src/pages/components/appbars/AppBar.jsx
+++ countries-react/src/pages/components/appbars/AppBar.jsx
@@ -1,8 +1,10 @@
 
 import { useContext } from "react";
 import StyleContext from "../../../context/style-context";
-import { MdOutlineDarkMode } from "react-icons/md";
 import { Link } from "react-router-dom";
+import { LuMoon } from "react-icons/lu";
+import { IoSunnyOutline } from "react-icons/io5";
+
 
 function AppBar() {
     let { isDarkMode, updateStyle } = useContext(StyleContext)
@@ -12,7 +14,7 @@ function AppBar() {
             <Link to='/'>
                 <h1 className="font-bold text-[2vi] " >  Where In The World?</h1>
             </Link>
-            <div className="font-bold flex justify-between "> {MdOutlineDarkMode} <button onClick={updateStyle}>{isDarkMode ? 'Light Mode' : 'Dark Mode'}</button></div>
+            <div className="font-bold flex items-center">{isDarkMode? <LuMoon/> :<IoSunnyOutline/>} <button onClick={updateStyle}>{isDarkMode ? 'Light Mode' : 'Dark Mode'}</button></div>
         </header>
     )
 }
