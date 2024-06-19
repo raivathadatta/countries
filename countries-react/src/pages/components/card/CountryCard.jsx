8368ae3d9d1387312949cd51f59de865b842bfb8
raivatha datta <raivatha.28.2@mountblue.tech>
2024-06-19 10:03:03 +0530

    added flag height

--- countries-react/src/pages/components/card/CountryCard.jsx
+++ countries-react/src/pages/components/card/CountryCard.jsx
@@ -13,7 +13,7 @@ function CountryCard({ countriesData }) {
             {countriesData.map((country) => (
                 <div key={country.name.common} className={`w-[22%] shadow-md my-[1%] rounded-xl  ${isDarkMode ? 'bg-elementDark text-textDark' : 'bg-elementLight text-textLight'}`} >
                     <Link to={`/${country.cca3}`} >
-                        <img src={country.flags.png} alt={country.name.common} className="rounded-xl" />
+                        <img src={country.flags.png} alt={country.name.common} className="rounded-xl h-[60%]" />
                         <div className="p-[3%]">
                             <h1 className="text-2xl font-bold my-[2%]">{country.name.common}</h1>
                             <p className="my-[1%]">
